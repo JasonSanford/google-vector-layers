3d8ef62149fabf1ae830c1b520943304990d4221
Jason Sanford <jasonsanford@gmail.com>
2011-04-19 05:15:56 +0000

    Adding script file.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: new google.maps.LatLng(39.5756, -105.0205),
+        zoom: 14
+    });
+});
