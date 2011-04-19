afd3e51e2502910677b34e4424a73b2995b4e3f4
Jason Sanford <jasonsanford@gmail.com>
2011-04-18 23:37:43 -0600

    Now showing map.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     var map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(39.5756, -105.0205),
-        zoom: 14
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     });
 });
