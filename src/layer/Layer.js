27818bd0faef22351c5bb3de2f6b5072698943d9
Jason Sanford <jasonsanford@gmail.com>
2011-11-27 15:03:35 -0700

    Adding MultiLineString and MultiPolygon support to _esriJsonGeometryToGoogle. Closes #20.

--- src/layer/Layer.js
+++ src/layer/Layer.js
@@ -314,35 +314,62 @@ gvector.Layer = gvector.Class.extend({
     _esriJsonGeometryToGoogle: function(geometry, opts) {
         var vector, vectors;
         if (geometry.x && geometry.y) {
-            // A simple point
+            // A Point
             opts.position = new google.maps.LatLng(geometry.y, geometry.x);
             vector = new google.maps.Marker(opts);
         } else if (geometry.points) {
+            // A MultiPoint
             vectors = [];
             for (var i = 0, len = geometry.points.length; i < len; i++) {
                 opts.position = new google.maps.LatLng(geometry.points[i].y, geometry.points[i].x);
                 vectors.push(new google.maps.Marker(opts));
             }
         } else if (geometry.paths) {
-            var path = [];
-            for (var i = 0; i < geometry.paths.length; i++) {
-                for (var i2 = 0; i2 < geometry.paths[i].length; i2++) {
-                    path.push(new google.maps.LatLng(geometry.paths[i][i2][1], geometry.paths[i][i2][0]));
+            if (geometry.paths.length > 1) {
+                // A MultiLineString
+                vectors = [];
+                for (var i = 0, len = geometry.paths.length; i < len; i++) {
+                    var path = [];
+                    for (var i2 = 0, len2 = geometry.paths[i].length; i2 < len2; i2++) {
+                        path.push(new google.maps.LatLng(geometry.paths[i][i2][1], geometry.paths[i][i2][0]));
+                    }
+                    opts.path = path
+                    vectors.push(new google.maps.Polyline(opts));
                 }
-            }
-            opts.path = path;
-            vector = new google.maps.Polyline(opts);
-        } else if (geometry.rings) {
-            var paths = [];
-            for (var i = 0; i < geometry.rings.length; i++) {
+            } else {
+                // A LineString
                 var path = [];
-                for (var i2 = 0; i2 < geometry.rings[i].length; i2++) {
-                    path.push(new google.maps.LatLng(geometry.rings[i][i2][1], geometry.rings[i][i2][0]));
+                for (var i = 0, len = geometry.paths[0].length; i < len; i++) {
+                    path.push(new google.maps.LatLng(geometry.paths[0][i][1], geometry.paths[0][i][0]));
+                }
+                opts.path = path;
+                vector = new google.maps.Polyline(opts);
+            }
+        } else if (geometry.rings) {
+            if (geometry.rings.length > 1) {
+                // A MultiPolygon
+                vectors = [];
+                for (var i = 0, len = geometry.rings.length; i < len; i++) {
+                    var paths = [];
+                    var path = [];
+                    for (var i2 = 0, len2 = geometry.rings[i].length; i2 < len2; i2++) {
+                        path.push(new google.maps.LatLng(geometry.rings[i][i2][1], geometry.rings[i][i2][0]));
+                    }
+                    paths.push(path);
+                    opts.paths = paths;
+                    vectors.push(new google.maps.Polygon(opts));
+                }
+            } else {
+                // A Polygon
+                var paths = [];
+                var path = [];
+                for (var i = 0, len = geometry.rings[0].length; i < len; i++) {
+                    path.push(new google.maps.LatLng(geometry.rings[0][i][1], geometry.rings[0][i][0]));
                 }
                 paths.push(path);
+                opts.paths = paths;
+                vector = new google.maps.Polygon(opts);
             }
-            opts.paths = paths;
-            vector = new google.maps.Polygon(opts);
         }
         return vector || vectors;
     },
