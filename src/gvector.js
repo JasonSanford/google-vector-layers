a71f5081a6c659afd1b44712575568880232ba00
Jason Sanford <jasonsanford@gmail.com>
2012-01-16 23:48:38 -0700

    Remove unused ROOT_URL constant in gvector.js.

--- src/gvector.js
+++ src/gvector.js
@@ -10,24 +10,6 @@
     root.gvector = {
         VERSION: '1.1.0',
 
-        /*ROOT_URL: (function () {
-            var scripts = document.getElementsByTagName('script'),
-                gvectorRe = /^(.*\/)gvector\-?([\w\-]*)\.js.*$/;
-
-            var i, len, src, matches;
-
-            for (i = 0, len = scripts.length; i < len; i++) {
-                src = scripts[i].src;
-                matches = src.match(gvectorRe);
-
-                if (matches) {
-                    if (matches[2] === 'include') { break; }
-                    return matches[1];
-                }
-            }
-            return '../../dist/';
-        }()),*/
-
         noConflict: function () {
             root.gvector = this._originalgvector;
             return this;
