d6b89b3509e5908fe2ded73ebf4e0f47fee4b042
Jason Sanford <jason.sanford@mapmyfitness.com>
2012-05-16 23:02:21 -0600

    Tabs to spaces

--- src/core/Class.js
+++ src/core/Class.js
@@ -9,62 +9,62 @@
 gvector.Class = function() {}; 
 
 gvector.Class.extend = function(/*Object*/ props) /*-> Class*/ {
-	
-	// extended class with the new prototype
-	var NewClass = function() {
-		if (this.initialize) {
-			this.initialize.apply(this, arguments);
-		}
-	};
+    
+    // extended class with the new prototype
+    var NewClass = function() {
+        if (this.initialize) {
+            this.initialize.apply(this, arguments);
+        }
+    };
 
-	// instantiate class without calling constructor
-	var F = function() {};
-	F.prototype = this.prototype;
-	var proto = new F();
-	
-	proto.constructor = NewClass;
-	NewClass.prototype = proto;
-	
-	// add superclass access
-	NewClass.superclass = this.prototype;
-	
-	// add class name
-	//proto.className = props;
-	
-	//inherit parent's statics
-	for (var i in this) {
-		if (this.hasOwnProperty(i) && i != 'prototype' && i != 'superclass') {
-			NewClass[i] = this[i];
-		}
-	}
+    // instantiate class without calling constructor
+    var F = function() {};
+    F.prototype = this.prototype;
+    var proto = new F();
+    
+    proto.constructor = NewClass;
+    NewClass.prototype = proto;
+    
+    // add superclass access
+    NewClass.superclass = this.prototype;
+    
+    // add class name
+    //proto.className = props;
+    
+    //inherit parent's statics
+    for (var i in this) {
+        if (this.hasOwnProperty(i) && i != 'prototype' && i != 'superclass') {
+            NewClass[i] = this[i];
+        }
+    }
 
-	// mix static properties into the class
-	if (props.statics) {
-		gvector.Util.extend(NewClass, props.statics);
-		delete props.statics;
-	}
-	
-	// mix includes into the prototype
-	if (props.includes) {
-		gvector.Util.extend.apply(null, [proto].concat(props.includes));
-		delete props.includes;
-	}
-	
-	// merge options
-	if (props.options && proto.options) {
-		props.options = gvector.Util.extend({}, proto.options, props.options);
-	}
+    // mix static properties into the class
+    if (props.statics) {
+        gvector.Util.extend(NewClass, props.statics);
+        delete props.statics;
+    }
+    
+    // mix includes into the prototype
+    if (props.includes) {
+        gvector.Util.extend.apply(null, [proto].concat(props.includes));
+        delete props.includes;
+    }
+    
+    // merge options
+    if (props.options && proto.options) {
+        props.options = gvector.Util.extend({}, proto.options, props.options);
+    }
 
-	// mix given properties into the prototype
-	gvector.Util.extend(proto, props);
-	
-	// allow inheriting further
-	NewClass.extend = arguments.callee;
-	
-	// method for adding properties to prototype
-	NewClass.include = function(props) {
-		gvector.Util.extend(this.prototype, props);
-	};
-	
-	return NewClass;
+    // mix given properties into the prototype
+    gvector.Util.extend(proto, props);
+    
+    // allow inheriting further
+    NewClass.extend = arguments.callee;
+    
+    // method for adding properties to prototype
+    NewClass.include = function(props) {
+        gvector.Util.extend(this.prototype, props);
+    };
+    
+    return NewClass;
 };
--- src/core/Util.js
+++ src/core/Util.js
@@ -7,21 +7,21 @@
  */
 
 gvector.Util = {
-	extend: function(/*Object*/ dest) /*-> Object*/ {	// merge src properties into dest
-		var sources = Array.prototype.slice.call(arguments, 1);
-		for (var j = 0, len = sources.length, src; j < len; j++) {
-			src = sources[j] || {};
-			for (var i in src) {
-				if (src.hasOwnProperty(i)) {
-					dest[i] = src[i];
-				}
-			}
-		}
-		return dest;
-	},
-	
-	setOptions: function(obj, options) {
-		obj.options = gvector.Util.extend({}, obj.options, options);
-	}
+    extend: function(/*Object*/ dest) /*-> Object*/ {    // merge src properties into dest
+        var sources = Array.prototype.slice.call(arguments, 1);
+        for (var j = 0, len = sources.length, src; j < len; j++) {
+            src = sources[j] || {};
+            for (var i in src) {
+                if (src.hasOwnProperty(i)) {
+                    dest[i] = src[i];
+                }
+            }
+        }
+        return dest;
+    },
+    
+    setOptions: function(obj, options) {
+        obj.options = gvector.Util.extend({}, obj.options, options);
+    }
 
 };
